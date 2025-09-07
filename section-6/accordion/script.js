b77bd7dfb63c8ea2860109ef5ae5fa767fbb410e
surajsuresh9 <49052225+surajsuresh9@users.noreply.github.com>
2025-09-07 19:43:49 +0530

    added more components

--- section-6/accordion/script.js
+++ section-6/accordion/script.js
@@ -1,12 +1,8 @@
-// console.log('hello');
-
 const buttons = document.getElementsByTagName('button');
 const sectionInfos = document.getElementsByClassName('info-section');
 const questionSections = document.getElementsByClassName('question-section');
 const numbers = document.getElementsByClassName('number');
-
-console.log(buttons);
-console.log(sectionInfos);
+const accContent = document.getElementsByClassName('accordion-content');
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
@@ -18,6 +14,7 @@ for (let i = 0; i < buttons.length; i++) {
             questionSections[i].style.fontWeight = '500';
             numbers[i].style.color = '#087f5b';
             numbers[i].style.fontWeight = '500';
+            accContent[i].style.borderTop = '4px solid #087f5b';
         } else if (buttons[i].textContent === '↑') {
             sectionInfos[i].style.display = 'none';
             buttons[i].innerHTML = '&darr;'
@@ -26,6 +23,7 @@ for (let i = 0; i < buttons.length; i++) {
             questionSections[i].style.fontWeight = '400';
             numbers[i].style.color = 'hsl(210, 10%, 83%)';
             numbers[i].style.fontWeight = '0';
+            accContent[i].style.borderTop = '';
         }
     })
 }
