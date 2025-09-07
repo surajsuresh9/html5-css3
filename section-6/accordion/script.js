2bc6def03c6dd5dfb5311df4a0de16bcc12f6b13
Suraj Suresh <sursures@publicisgroupe.net>
2025-09-07 12:15:48 +0530

    initial commit

---
+++ section-6/accordion/script.js
@@ -0,0 +1,31 @@
+// console.log('hello');
+
+const buttons = document.getElementsByTagName('button');
+const sectionInfos = document.getElementsByClassName('info-section');
+const questionSections = document.getElementsByClassName('question-section');
+const numbers = document.getElementsByClassName('number');
+
+console.log(buttons);
+console.log(sectionInfos);
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+        if (buttons[i].textContent === '↓') {
+            sectionInfos[i].style.display = 'block';
+            buttons[i].innerHTML = '&uarr;'
+
+            questionSections[i].style.color = '#087f5b';
+            questionSections[i].style.fontWeight = '500';
+            numbers[i].style.color = '#087f5b';
+            numbers[i].style.fontWeight = '500';
+        } else if (buttons[i].textContent === '↑') {
+            sectionInfos[i].style.display = 'none';
+            buttons[i].innerHTML = '&darr;'
+
+            questionSections[i].style.color = 'hsl(210, 10%, 23%)';
+            questionSections[i].style.fontWeight = '400';
+            numbers[i].style.color = 'hsl(210, 10%, 83%)';
+            numbers[i].style.fontWeight = '0';
+        }
+    })
+}
